a8a93e86ad0a21bc9725aa4f3bc4ac1e53f1d2f7
Eli Reiners <goelyukim@gmail.com>
2020-06-11 20:14:53 -0400

    Add logging

--- app.js
+++ app.js
@@ -19,6 +19,9 @@ app.use(function validateBearerToken(req, res, next){
     const authToken = req.get('Authorization')
     const apiToken = process.env.API_TOKEN;
 
+    console.log(authToken);
+    console.log(apiToken);
+    
     if (!authToken || authToken.split(' ')[1] !== apiToken ){
        return res.status(401).json({error: 'Unauthorized request'})
     }
